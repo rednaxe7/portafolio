885e45e8bae3737ec7004d4e37869a657b17e405
Alexander <rednaxe7@gmail.com>
2018-11-02 23:44:21 -0400

    Actualizacion Completa del Portafolio

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Portafolio
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.0.4.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.2.5.
 
 ## Development server
 
---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+	//info: any = {};
+	info: InfoPagina = {};
+	cargada = false;
+
+  constructor( private http: HttpClient) { 
+
+  	//console.log('Servicio de InfoPagina cargado.');  	
+
+  	//Leer el archivo JSON
+  	this.http.get('assets/data/data-pagina.json')
+  		.subscribe((resp: InfoPagina) => {
+
+  			
+  			this.cargada = true;
+  			this.info = resp;
+  			console.log(resp);
+  			console.log(resp['email']);
+			
+
+  		});
+
+  }
+}
