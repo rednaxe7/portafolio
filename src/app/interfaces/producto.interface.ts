c7f5f0d04ddc38d8b2a55903fbd90ada259ed601
Alexander <rednaxe7@gmail.com>
2018-11-10 00:07:20 -0400

    Creación de Servicio para Manejo de Json Productos en Html

---
+++ src/app/interfaces/producto.interface.ts
@@ -0,0 +1,10 @@
+declare module namespace {
+
+    export interface Producto{
+        categoria: string;
+        cod: string;
+        titulo: string;
+        url: string;
+    }
+
+}
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -7,26 +7,41 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 })
 export class InfoPaginaService {
 
-	//info: any = {};
-	info: InfoPagina = {};
-	cargada = false;
+ //info: any = {};
+ info: InfoPagina = {};
+ cargada = false;
+ equipo: any[] = [];
 
-  constructor( private http: HttpClient) { 
+ constructor( private http: HttpClient) { 
+ 	this.cargarInfo();
+	this.cargarEquipo();
+ } 
 
-  	//console.log('Servicio de InfoPagina cargado.');  	
+ private cargarInfo(){
+        //Leer el archivo JSON
+        this.http.get('assets/data/data-pagina.json')
+                .subscribe((resp: InfoPagina) => {
 
-  	//Leer el archivo JSON
-  	this.http.get('assets/data/data-pagina.json')
-  		.subscribe((resp: InfoPagina) => {
 
-  			
-  			this.cargada = true;
-  			this.info = resp;
-  			console.log(resp);
-  			console.log(resp['email']);
-			
+                        this.cargada = true;
+                        this.info = resp;
+			// console.log(resp);
+                        console.log(resp['email']);
+                
+                });
+	
+ }
 
-  		});
+ private cargarEquipo(){
+        //Leer JSON Firebase
+        this.http.get('https://angular-html-e020b.firebaseio.com/equipo.json')
+                .subscribe( (resp: any[])  => {
 
-  }
-}
+			this.equipo = resp;
+			//console.log(resp);
+
+                });
+
+ }
+
+} 
