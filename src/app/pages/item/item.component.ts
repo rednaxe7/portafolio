d379fbf375a82a2910f30f9f2ae1f326dcee2d17
Alexander <rednaxe7@gmail.com>
2018-11-15 23:02:42 -0400

    Actualización de las imagenes en los productos

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -11,6 +11,9 @@ import { ProductoDescripcion } from '../../interfaces/producto-descripcion.inter
 
 export class ItemComponent implements OnInit {
 
+ producto: ProductoDescripcion;
+ id: string;
+
   constructor( private route: ActivatedRoute, 
 	       public productoService: ProductosService ) { }
 
@@ -21,7 +24,10 @@ export class ItemComponent implements OnInit {
 			//console.log(parametros['id']);
 			this.productoService.getProducto(parametros['id'])
 			     .subscribe( (producto: ProductoDescripcion)  => {
-				console.log(producto);
+				this.id = parametros['id'];
+				this.producto = producto;
+				//console.log(producto);
+
 			     });
 		});
   }
